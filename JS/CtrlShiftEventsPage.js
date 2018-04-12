dd190173b731f37b619352698936454652cfd55c
kennanstanley-bey <kstanleybey@yahoo.com>
2018-04-12 12:33:26 +0200

    created new JS and CSS files for code organization

--- JS/CtrlShiftEventsPage.js
+++ JS/CtrlShiftEventsPage.js
@@ -1,18 +1,20 @@
+//Collapse nav menu when link is clicked
 $(function() {
   $("div.collapse ul.nav li a").each(function() {
     $(this).on("click", function() {
       var $obj = $($(this).parents(".show")[0]);
       $obj.animate({height: "1px"}, function(){
         $obj.removeClass("show").addClass("collapse");
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
 
+//Prevents language dropdown menu from staying open after click
 $('.dropdown').on('show.bs.dropdown', function(event) {
     event.preventDefault();
 });
 $('.dropdown').on('hide.bs.dropdown-menu', function(event) {
     event.preventDefault();
-});
+})
 
