39e59d0a9e4a2844648a88a9997b5f8c2300c8d7
kennanstanley-bey <kstanleybey@yahoo.com>
2018-04-16 10:26:54 +0200

    added favicon-32x32.png to git

--- JS/fullcalendarEdit.js
+++ JS/fullcalendarEdit.js
@@ -4,17 +4,17 @@ $(function() {
         googleCalendarApiKey: 'AIzaSyCbWq0s2_RJHTnJ3zhq1Q5H3eIazUMC_yQ',
         events: {
           googleCalendarId: 'rjm3qsnc0uv7cgpmv8236nikc4@group.calendar.google.com',
-          className: 'event-text'
+//          className: 'event-text'
         },
         fixedWeekCount: false,
-        eventRender: function(event,element) {
-          element.popover({
-            animation: true,
-            delay: 300,
-            content: '<b>Inicio</b>:'+event.start+"<b>Fin</b>:"+event.end,
-            trigger: 'hover'
-          });
-        },
+//        eventRender: function(event,element) {
+//          element.popover({
+//            animation: true,
+//            delay: 300,
+//            content: '<b>Inicio</b>:'+event.start+"<b>Fin</b>:"+event.end,
+//            trigger: 'hover'
+//          });
+//        },
         header: {
           left: 'prev,next today',
           center: 'title',
